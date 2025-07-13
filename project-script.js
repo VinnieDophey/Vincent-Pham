f92721d4de547e5c7b310f92305f5efa3aab7be4
VinnieDophey <130087284+VinnieDophey@users.noreply.github.com>
2025-07-13 14:56:08 -0400

    Stuff

--- project-script.js
+++ project-script.js
@@ -1,26 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-  gsap.registerPlugin(ScrollTrigger);
-
-  const img = document.querySelector(".img");
-  const container = document.querySelector(".img-container");
-
-  // Calculate how much we want to reveal (20% of viewport height)
-  const revealAmount = window.innerHeight;
-
-  // Set initial crop (shows top 80% of image)
-
-  // Animate to reveal bottom 20%
-  gsap.to(img, {
-    y: -revealAmount, // Negative moves image up
-    ease: "none",
-    scrollTrigger: {
-      trigger: "main",
-      start: "top bottom",
-      end: "bottom top",
-      scrub: true,
-    },
-  });
-});
+// Replace your current scroll event listener with this:
 let lastScrollTop = 0;
 const header = document.querySelector(".header");
 const headerHeight = header.offsetHeight;
